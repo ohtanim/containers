b17b2783d3360d69bf97b14ba9fbe72c4e37de95
Munetaka Ohtani <ohtanim@jp.ibm.com>
2025-07-05 15:14:22 +0900

    add ubi

---
+++ ubi09/run.sh
@@ -0,0 +1,1 @@
+docker run -v ./shared:/shared --platform linux/amd64 --net=host -it docker.io/library/ubi9-py311 bash
---
+++ ubi10/run.sh
@@ -0,0 +1,1 @@
+docker run -v ./shared:/shared --net=host -it docker.io/library/ubi10 bash
