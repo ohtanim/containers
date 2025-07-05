9539b8b75317ce4ebb18b352d8759507688c0316
Munetaka Ohtani <ohtanim@jp.ibm.com>
2025-07-05 15:01:47 +0900

    update almalinux

--- almalinux/run.sh
+++ almalinux/run.sh
@@ -1,1 +1,1 @@
-docker run -v ./shared:/shared --platform linux/amd64 --net=host -it direct-access-almalinux-py312 bash
+docker run -v ./shared:/shared --platform linux/amd64 --net=host -it almalinux-py312 bash
